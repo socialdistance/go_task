f28f06ddf557984ae2ea1bbe53869863ef97e013
socialdistance <pleaseuseurbrain@protonmail.com>
2023-04-25 17:48:01 +0300

    commit

--- go_request_task/main.go
+++ go_request_task/main.go
@@ -1,50 +1,63 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
-	"syscall"
 	"time"
 )
 
 type Result struct {
 	Response *http.Response
 	Error    error
-	Status   string
+	URL      string
 	// add retry field ?
 }
 
 // Ретраи перекладывать в другую горутину и делать запросы в другой горутине ?
 // Писать 200 ОК в один канал, при попытках ретарая писать результат в другой канал, после слить их в один и вернуть
 func checkStatus(done <-chan struct{}, urls ...string) <-chan Result {
+	client := &http.Client{Timeout: 2 * time.Second}
+
 	resultsSuccesfull := make(chan Result)
+	resultsFailed := make(chan Result)
 
 	go func() {
 		defer close(resultsSuccesfull)
 		for _, url := range urls {
-			var result Result
-			client := &http.Client{Timeout: 2 * time.Second}
+			var resultSuccess, resultFailed Result
 			resp, err := client.Get(url)
-			if errors.Is(err, syscall.ECONNREFUSED) {
-				fmt.Println("Timeout:", url)
-				result = Result{Error: err, Response: nil}
-				continue
-			}
+			fmt.Println("err", err)
 			if err != nil {
-				result = Result{Error: err, Response: nil}
+				resultFailed = Result{Response: nil, Error: err, URL: url}
+				fmt.Println("failed", resultFailed)
 			} else {
-				result = Result{Error: nil, Response: resp}
+				resultSuccess = Result{Response: resp, Error: nil, URL: url}
+				fmt.Println("success", resultSuccess)
 			}
+
 			select {
-			case <-done:
-				return
-			case resultsSuccesfull <- result:
+			// case <-done:
+			// 	return
+			case resultsSuccesfull <- resultSuccess:
+			case resultsFailed <- resultFailed:
 			}
 		}
 	}()
 
+	// for retry requests
+	go func() {
+		defer close(resultsFailed)
+		for urlFailed := range resultsFailed {
+			resp, err := client.Get(urlFailed.URL)
+			// if err != nil {
+
+			// }
+			fmt.Println("SECOND", resp, err)
+		}
+	}()
+
 	// <-resultsFailed
+	// fmt.Println("SS", resultsSuccesfull)
 
 	return resultsSuccesfull
 }
@@ -53,7 +66,8 @@ func main() {
 	done := make(chan struct{})
 	defer close(done)
 
-	urls := []string{"https://www.bbas.com", "https://badhost", "https://www.avito.ru/", "https://www.ozon.ru/", "https://vk.com/", "https://yandex.ru/", "https://www.google.com/", "https://github.com/", "http://medium.com/", "https://golang.org/"}
+	// "https://someOtherSite.com", "https://www.bbas.com", "https://badhost",
+	urls := []string{"https://www.avito.ru/", "https://www.ozon.ru/", "https://vk.com/", "https://yandex.ru/", "https://www.google.com/", "https://github.com/", "http://medium.com/", "https://golang.org/"}
 	for response := range checkStatus(done, urls...) {
 		if response.Error != nil {
 			fmt.Printf("error: %v\n", response.Error)
