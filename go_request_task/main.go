9a0889d1e31eff7ba0d7e9d14a6e0d5912cabd02
socialdistance <pleaseuseurbrain@protonmail.com>
2023-04-25 21:42:26 +0300

    retry

--- go_request_task/main.go
+++ go_request_task/main.go
@@ -15,7 +15,8 @@ type Result struct {
 
 // Ретраи перекладывать в другую горутину и делать запросы в другой горутине ?
 // Писать 200 ОК в один канал, при попытках ретарая писать результат в другой канал, после слить их в один и вернуть
-func checkStatus(done <-chan struct{}, urls ...string) <-chan Result {
+func checkStatus(done chan struct{}, urls ...string) <-chan Result {
+	retry := 3
 	client := &http.Client{Timeout: 2 * time.Second}
 
 	resultsSuccesfull := make(chan Result)
@@ -26,7 +27,6 @@ func checkStatus(done <-chan struct{}, urls ...string) <-chan Result {
 		for _, url := range urls {
 			var resultSuccess, resultFailed Result
 			resp, err := client.Get(url)
-			fmt.Println("err", err)
 			if err != nil {
 				resultFailed = Result{Response: nil, Error: err, URL: url}
 				fmt.Println("failed", resultFailed)
@@ -35,6 +35,8 @@ func checkStatus(done <-chan struct{}, urls ...string) <-chan Result {
 				fmt.Println("success", resultSuccess)
 			}
 
+			// Запросы продолжают делаться в горутине для ретраев потому что приходит
+			// {nil nil}, а это тип Result с nil в полях
 			select {
 			// case <-done:
 			// 	return
@@ -46,18 +48,26 @@ func checkStatus(done <-chan struct{}, urls ...string) <-chan Result {
 
 	// for retry requests
 	go func() {
+		var result Result
 		defer close(resultsFailed)
 		for urlFailed := range resultsFailed {
-			resp, err := client.Get(urlFailed.URL)
-			// if err != nil {
-
-			// }
-			fmt.Println("SECOND", resp, err)
+			for i := 0; i < retry; i++ {
+				resp, err := client.Get(urlFailed.URL)
+				if err != nil {
+					fmt.Printf("WARNING: Error job in attempt no %d: %s - retrying...\n", i+1, err)
+					continue
+				} else {
+					result = Result{Response: resp, Error: err}
+				}
+				select {
+				case resultsSuccesfull <- result:
+				default:
+					fmt.Println("Default")
+				}
+			}
 		}
-	}()
 
-	// <-resultsFailed
-	// fmt.Println("SS", resultsSuccesfull)
+	}()
 
 	return resultsSuccesfull
 }
@@ -67,8 +77,9 @@ func main() {
 	defer close(done)
 
 	// "https://someOtherSite.com", "https://www.bbas.com", "https://badhost",
-	urls := []string{"https://www.avito.ru/", "https://www.ozon.ru/", "https://vk.com/", "https://yandex.ru/", "https://www.google.com/", "https://github.com/", "http://medium.com/", "https://golang.org/"}
+	urls := []string{"https://badhost", "https://www.avito.ru/", "https://www.ozon.ru/", "https://vk.com/", "https://yandex.ru/", "https://www.google.com/", "https://github.com/", "http://medium.com/", "https://golang.org/"}
 	for response := range checkStatus(done, urls...) {
+		time.Sleep(5 * time.Second)
 		if response.Error != nil {
 			fmt.Printf("error: %v\n", response.Error)
 			continue
