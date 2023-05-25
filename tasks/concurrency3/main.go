6bcd8e36628fe89213adce9da1ade36afda9d259
socialdistance <pleaseuseurbrain@protonmail.com>
2023-05-25 16:04:42 +0300

    f

--- tasks/concurrency3/main.go
+++ tasks/concurrency3/main.go
@@ -17,28 +17,35 @@ type ringBuffer struct {
 }
 
 func (r *ringBuffer) Run() {
-	// go func() {
+	size := cap(r.outCh)
 	for v := range r.inCh {
-		v := v
-		fmt.Println(v)
+		if len(r.outCh) == size {
+			<-r.outCh
+		}
 		r.outCh <- v
 	}
-	// }()
+
+	close(r.outCh)
 }
 
 func main() {
 	inCh := make(chan int)
-	outCh := make(chan int)
+	outCh := make(chan int, 4)
 	rb := NewRingBuffer(inCh, outCh)
+
 	go rb.Run()
+	resSlice := make([]int, 0)
 
 	max := 100
 	for i := 0; i < max; i++ {
 		inCh <- i
 	}
-	resSlice := make([]int, 0)
+
+	close(inCh)
+
 	for res := range outCh {
 		resSlice = append(resSlice, res)
 	}
+
 	fmt.Println(resSlice)
 }
